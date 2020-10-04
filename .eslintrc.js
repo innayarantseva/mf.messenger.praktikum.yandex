64995875067f7f35156b4523046000c74321805a
Inna Lytkina <innayarantseva@gmail.com>
2020-10-04 18:05:58 +0300

    #34: added eslint and stylelint; audited pachages

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "parser": "@typescript-eslint/parser",
+    "plugins": ["@typescript-eslint"],
+    "rules": {
+        "no-unused-vars": 2,
+        "@typescript-eslint/no-unused-vars": 2,
+    }
+}
---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // "extends": "stylelint-config-standard",
+    "rules": {
+        "color-hex-case": "upper"
+    },
+    "ignoreFiles": [
+        "dist/*",
+        "coverage/*"
+    ]
+}

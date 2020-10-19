a334e003da1bd4139bd43619f6d5762025f313b4
Inna Lytkina <innayarantseva@gmail.com>
2020-10-19 23:32:22 +0300

    Review fixes: fixed cloneDeep tests

--- src/utils/mydash/deepClone.spec.ts
+++ src/utils/mydash/deepClone.spec.ts
@@ -5,5 +5,12 @@ const objWithArray = { a: [{ b: 1 }] };
 
 test('cloning an object not to be equal its copy', () => {
     expect(cloneDeep(obj1)).not.toBe(obj1);
+});
+
+test('cloning an object with array not to be equal its copy', () => {
     expect(cloneDeep(objWithArray)).not.toBe(objWithArray);
+});
+
+test('cloning an object preservers its initial structure', () => {
+    expect(cloneDeep(objWithArray)).toEqual(objWithArray);
 });
